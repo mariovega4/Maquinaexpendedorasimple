81ec6f19b1c099db56de7eda90de0aaef6bf5c3f
mario vega <mariovegasantos3@gmail.com>
2024-10-23 14:18:06 +0200

    tercer commit

--- MaquinaExpendedoraSimple.java
+++ MaquinaExpendedoraSimple.java
@@ -16,8 +16,8 @@ public class MaquinaExpendedoraSimple {
      * precio del billete y el origen y destino dados. Se asume que el precio
      * del billete que se recibe es mayor que 0.
      */
-    public MaquinaExpendedoraSimple(int precioDelBillete, String origen, String destino) {
-        precioBillete = precioDelBillete= 20;
+    public MaquinaExpendedoraSimple() {
+        precioBillete = 20;
         balanceClienteActual = 0;
         totalDineroAcumulado = 0;
         estacionOrigen = "León";
